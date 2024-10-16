53dea8be8bc982ee9c7d6e0c61eef3d017c1456f
Insoft Developers <irdn.software@gmail.com>
2024-10-16 22:33:16 +0700

    finish

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,11 +3,11 @@ class Constant {
   // static String BASE_URL = "http://192.168.100.3/akunting-ukm/public/";
   // static String ROOT_URL = "http://192.168.100.3/randu-api/";
   // static String BASE_URL = ROOT_URL + "public/";
-  static String BASE_URL = "https://dev.randu.co.id/";
+  static String BASE_URL = "https://app.randu.co.id/";
   // ignore: non_constant_identifier_names
   // static String JOURNAL_REPORT = ROOT_URL + "storage/app/";
   static String JOURNAL_REPORT = BASE_URL + "storage/";
 
-  static String VERSION = "1.0.2";
-  static String RELEASE_DATE = "14 Oktober 2024";
+  static String VERSION = "1.0.3";
+  static String RELEASE_DATE = "16 Oktober 2024";
 }
